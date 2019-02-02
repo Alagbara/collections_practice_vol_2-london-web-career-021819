3dbec6db390dc58a62a57909815a7b8fffa6e8ec
Moses Olawoyin <m.bastford@gmail.com>
2019-02-02 00:15:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
-# your code goes here
+def begins_with_r(array)
+  result = true
+  array.each do|element|
+  if !element.start_with?("r")
+   result = false 
+  end
+  
+  end
+  result
+end
+
+def contain_a(array)
+   array.select do |word|
+  word.include?("a")
+  end
+end
+
+def first_wa(array)
+  letter_wa = []
+  array.each do |letter|
+   if letter.to_s.start_with?("wa")
+     letter_wa << letter
+   end
+  end
+  letter_wa.first
+end
+
+def remove_non_strings(array)
+  array.grep(String)
+end
+
+def count_elements(array)
+  array.group_by(&:itself).map{|k,v|k.merge(count:v.length)}
+end
+
+def merge_data(array)
+  v1[0].values.map.with_index {|v, i| v2[i].merge(v)}
+end
